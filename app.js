2bce4d07c3c36dc92baa3afa79791e60896c58e3
Antonio Carlos <jr_acn@yahoo.com.br>
2018-04-13 19:24:45 -0300

    fim do projeto

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const webconfig = require('./webconfig.js');
 
 app.use(require('cors')());
 app.use(express.static(__dirname));
@@ -12,20 +11,22 @@ app.get("/products/", (req, res, next) => {
             "description": "Vídeogame Playstation 4 HD de 500gb",
             "details": "O sistema PlayStation®4 redefine a jogabilidade com riqueza e envolvimento, além de velocidade e gráficos poderosos",
             "like": false,
-            "img": "playstation.png"
+            "img": "playstation.png",
+            "preco": "4100",
         },
         {
             "title": "Xbox 360",
             "description": "2 Controles + 5 Jogos",
             "details": "Experimente uma nova geração de jogos e entretenimento com Xbox",
             "like": false,
-            "img": "xbox.jpg"
+            "img": "xbox.jpg",
+            "preco": "3600",
         }
     ]);
 })
 
-app.listen(webconfig.myApi.port, () => {
-    console.log(`[${webconfig.myApi.name}] - Ativo :D | ${webconfig.myApi.url}:${webconfig.myApi.port}`);
+app.listen(process.env.port || 8050, () => {
+    console.log(`[API] - Ativo :D | http://localhost:8050`);
 });
 
 module.exports = app;
